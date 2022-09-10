ddeab4205fa85f0397f0a54ee42c9feebee5aa4c
Miracle C. Ezejiofor <mimiezejiofor1@gmail.com>
2022-09-09 19:14:41 -0700

    Cgit add .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ int main (void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-        {
+	{
 		printf("%d is positive\n", n);
 	} else if (n == 0)
 	{
@@ -26,4 +26,3 @@ int main (void)
 	}
 	return (0);
 }
-										{
