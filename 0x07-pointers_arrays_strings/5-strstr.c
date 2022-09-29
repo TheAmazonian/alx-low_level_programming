1d46e1bc306e7917c02c5e4de03ec5586b9fce16
Miracle C. Ezejiofor <mimiezejiofor1@gmail.com>
2022-09-28 22:14:18 -0700

    Pointers, Arrays and Strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,40 @@
 #include "main.h"
+#define NULL 0
 
 /**
- * _strstr - locates substring
- * @haystack: string to locate
- * @needle: substring to locate
- * Return: returns string at starting sub or null
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: Returns 0
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
+	int i = 0, j, x;
 
 	if (needle[0] == '\0')
 		return (haystack);
 
-		for (i = 0; haystack[i] != '\0'; i++)
+		while (haystack[i] != '\0') /* iterate through haystack */
 		{
+			/* if a byte matches first char of needle */
+			/* interate through needle until match ends */
 			if (haystack[i] == needle[0])
 			{
-				for (j = 0; needle[j] != '\0'; j++)
+				x = i, j = 0;
+				while (needle[j] != '\0')
 				{
-					if (haystack[i + j] != needle[j])
+					if (haystack[x] == needle[j])
+						x++, j++;
+					else
 						break;
-				}
+				} /* if matched throughout, return haystack */
 				if (needle[j] == '\0')
+				{
 					return (haystack + i);
+				}
 			}
-
+			i++;
 		}
-		return ('\0');
+		return (NULL);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,25 +1,9 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
 
-/**
- *_memset() function - fills the first n bytes of the memory area pointed to by s with the constant byte b
- *@s: memory area
- *@b: constant byte
- *
- *Return: A pointer to the memory area
- */
-
 char *_memset(char *s, char b, unsigned int n);
-
-/**
- *_memcpy() function - copies n bytes from memory area src to memory area dest
- *@dest: destination memory area
- *@src: source of byte to copy
- *
- *Return: A pointer to dest
- */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
@@ -28,13 +12,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
-
-/**
- * _strstr - locates substring
- * @haystack: string to locate substring
- * @needle: string to locate
- * Return: return string found
- */
 
 char *_strstr(char *haystack, char *needle);
 
