bd902114fcc3b541c269a8840372e39ab7c380e1
Miracle C. Ezejiofor <mimiezejiofor1@gmail.com>
2022-09-28 22:35:58 -0700

    Pointers, Arrays and Strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #define NULL 0
-#include <stdio.h>
 
 /**
  * _strstr - locate and return pointer to first occurence of substring
@@ -16,26 +15,26 @@ char *_strstr(char *haystack, char *needle)
 	if (needle[0] == '\0')
 		return (haystack);
 
-		while (haystack[i] != '\0') /* iterate through haystack */
+	while (haystack[i] != '\0') /* iterate through haystack */
+	{
+		/* if a byte matches first char of needle */
+		/* interate through needle until match ends */
+		if (haystack[i] == needle[0])
 		{
-			/* if a byte matches first char of needle */
-			/* interate through needle until match ends */
-			if (haystack[i] == needle[0])
+			x = i, j = 0;
+			while (needle[j] != '\0'
 			{
-				x = i, j = 0;
-				while (needle[j] != '\0')
-				{
-					if (haystack[x] == needle[j])
-						x++, j++;
-					else
-						break;
-				} /* if matched throughout, return haystack */
-				if (needle[j] == '\0')
-				{
-					return (haystack + i);
-				}
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			} /* if matched throughout, return haystack */
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
 			}
-			i++;
 		}
-		return (NULL);
+		i++;
+	}
+	return (NULL);
 }
