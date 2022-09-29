8a938cad813a7f8793e916d0ad0080fe837157fb
Miracle C. Ezejiofor <mimiezejiofor1@gmail.com>
2022-09-28 22:45:44 -0700

    Pointers, Arrays and Strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[0])
 		{
 			x = i, j = 0;
-			while (needle[j] != '\0'
+			while (needle[j] != '\0')
 			{
 				if (haystack[x] == needle[j])
 					x++, j++;
